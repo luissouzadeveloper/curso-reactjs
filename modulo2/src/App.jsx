e28c9cfa8eff1158be7ba5fb74e1c36340a27f39
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-07 09:12:20 -0300

    Criação e exportação de componentes - React

---
+++ modulo2/src/App.jsx
@@ -0,0 +1,16 @@
+// CSS
+import './App.css'
+
+// Componentes
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <>
+      <h1>Olá, mundo!</h1>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
