192fa99ace940e2e78d63decfc3ce1de0f1bc85a
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-23 21:28:58 -0300

    CSS de componente

--- modulo4/src/App.jsx
+++ modulo4/src/App.jsx
@@ -1,11 +1,19 @@
 // CSS
 import './App.css'
 
+// Componentes
+import MyComponent from './components/MyComponent';
+
 function App() {
 
   return (
     <>
+      {/* CSS global */}
       <h1>React com css</h1>
+
+      {/* CSS de componente */}
+      <MyComponent />
+      <p className='my-p'>Esse parágrafo é de App.jsx</p>
     </>
   )
 }
---
+++ modulo4/src/components/MyComponent.jsx
@@ -0,0 +1,15 @@
+// CSS
+import './MyComponent.css';
+
+const MyComponent = () => {
+    return (
+        <>
+            <h2>CSS de componente</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias recusandae, dolorum rem consequatur aspernatur itaque illum veritatis praesentium eos exercitationem, adipisci, culpa hic temporibus voluptatum quod laudantium placeat magnam odit.
+            </p>
+        </>
+    )
+}
+
+export default MyComponent
