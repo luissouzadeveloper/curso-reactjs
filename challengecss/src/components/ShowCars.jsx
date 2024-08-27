8109726e46d6497d6f580aca643b6512ce7fb372
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-26 21:55:18 -0300

    Mudanças

--- challengecss/src/components/ShowCars.jsx
+++ challengecss/src/components/ShowCars.jsx
@@ -4,11 +4,13 @@ import styles from './ShowCars.module.css'
 const ShowCars = ({name, brand, year}) => {
     return (
         <>
-            <ul>
-                <li className={styles.list_li}>Nome: {name}</li>
-                <li className={styles.list_li}>Marca: {brand}</li>
-                <li className={styles.list_li}>Ano: {year}</li>
-            </ul>
+            <div className={styles.container}>
+                <ul className={styles.container_ul}>
+                    <li className={styles.list_li}>Nome: {name}</li>
+                    <li className={styles.list_li}>Marca: {brand}</li>
+                    <li className={styles.list_li}>Ano: {year}</li>
+                </ul>
+            </div>
         </>
     )
 }
