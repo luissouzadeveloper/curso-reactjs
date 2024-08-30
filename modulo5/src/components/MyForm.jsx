5d45733f34556c0276feb43f9a1fc81378dcfd7d
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-30 16:53:38 -0300

    Código limpo com label e gerenciamento de dados no input

--- modulo5/src/components/MyForm.jsx
+++ modulo5/src/components/MyForm.jsx
@@ -1,15 +1,36 @@
 // CSS
 import './MyForm.css'
 
+// Hooks
+import { useState } from 'react'
+
 const MyForm = () => {
+
+    // 3- Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (event) => {
+        setName(event.target.value);
+    };
+
+    console.log(name);
+
     return (
         <>
             {/* 1- Criação de form */}
             <form>
                 <div>
                     <label htmlFor="name">Nome: </label>
-                    <input type="text" name='name' placeholder='Digite seu nome' />
+                    <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
                 </div>
+
+                {/* 2- label envolvendo input */}
+                <label>
+                    <span>E-mail:</span>
+                    <input type="email" name="email" placeholder='Digite seu nome' />
+                </label>
+
                 <input type="submit" value="Enviar" />
             </form>
         </>
