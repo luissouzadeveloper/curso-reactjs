b8bd952e9d3aa2fcf4052ad72d247433985b96f2
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-11 21:31:06 -0300

    Tela incial do jogo

---
+++ secret-word/src/App.jsx
@@ -0,0 +1,18 @@
+// CSS
+import './App.css'
+
+// Componentes
+import StartScreen from './components/StartScreen'
+
+function App() {
+
+  return (
+    < >
+      <div className='App'>
+        <StartScreen />
+      </div>
+    </>
+  )
+}
+
+export default App
